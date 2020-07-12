fefd84824d68a37362016336f1a384014d1f95d3
TsapkoZh <60929704+TsapkoZh@users.noreply.github.com>
2020-07-12 23:32:38 +0300

    fix

---
+++ build/precache-manifest.fbd9a5f02b797b3327f53ef22d4b22c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "903ca3cd2458276498b58dbd9918984a",
+    "url": "/todoListRR/index.html"
+  },
+  {
+    "revision": "b8cfc5ad7aceeb7d0a60",
+    "url": "/todoListRR/static/css/main.b8c2feb2.chunk.css"
+  },
+  {
+    "revision": "761477cf3e0a5864b4c6",
+    "url": "/todoListRR/static/js/2.724e1d9c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todoListRR/static/js/2.724e1d9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8cfc5ad7aceeb7d0a60",
+    "url": "/todoListRR/static/js/main.8a9928cc.chunk.js"
+  },
+  {
+    "revision": "1733c20a07a02f9de0b9",
+    "url": "/todoListRR/static/js/runtime-main.6834733c.js"
+  }
+]);
