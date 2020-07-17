7b8c67fd3b60985f3778aaa407d195e9a89505ff
TsapkoZh <60929704+TsapkoZh@users.noreply.github.com>
2020-07-17 16:05:04 +0300

    Updates

--- precache-manifest.eea3b45424a290ede6af693ebeec2614.js
+++ precache-manifest.eea3b45424a290ede6af693ebeec2614.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "40b33c708430249861bc497669570e9e",
+    "revision": "e3d1349eca3dc8d848cee871ed8fee78",
     "url": "/todoListRR/index.html"
   },
   {
-    "revision": "83c34e04ddb698b7e011",
-    "url": "/todoListRR/static/css/main.b2a1617f.chunk.css"
+    "revision": "5c6ef7b929e1682db86d",
+    "url": "/todoListRR/static/css/main.92db25c8.chunk.css"
   },
   {
     "revision": "761477cf3e0a5864b4c6",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/todoListRR/static/js/2.724e1d9c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "83c34e04ddb698b7e011",
-    "url": "/todoListRR/static/js/main.a2c69cd6.chunk.js"
+    "revision": "5c6ef7b929e1682db86d",
+    "url": "/todoListRR/static/js/main.705b987d.chunk.js"
   },
   {
     "revision": "1733c20a07a02f9de0b9",
